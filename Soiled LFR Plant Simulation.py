1ac3249451336ac9f0f9b20953ebce33cc76b71f
Oscar Djuric <oscar.djuric@gmail.com>
2024-12-11 11:56:39 +1000

    Tracking the soiling of the heliostats over the whole year

--- Soiled LFR Plant Simulation.py
+++ Soiled LFR Plant Simulation.py
@@ -1,3 +1,4 @@
+#%%
 """
 Integrating the soiling model from HelioSoil to the LFR plant design.
 This script attempts to model the efficiency of the plant across the whole year (2018) from the data collected in HelioSoil.
@@ -31,7 +32,6 @@ import soiling_model.base_models as smb
 
 from raytracing_soiling_functions import calculate_theta_aim
 from raytracing_soiling_functions import calculate_tilt
-from raytracing_soiling_functions import calculate_panel_normal
 
 # LFR Plant Setup
 lat, lon = -31.2,136.816667
@@ -47,13 +47,7 @@ panel_height = 0.5       # [m]
 panel_spacing = 0.2      # [m]
 panel_positions = np.arange(-3.5, 3.75, panel_width + panel_spacing) # Describes the x coordinate for each of the mirros, 
 num_heliostats = len(panel_positions)                                # ranging from -3.5m to 3.75m.
-slope_error = 0.1        # [mrad]
-specularity_error = 0.1  # [mrad]
 
-stg1_height = receiver_height
-stg1_length = panel_length 
-stg1_width = panel_width * len(panel_positions) + panel_spacing * (len(panel_positions) - 1) 
-distance_multiplier = 500 # Scaling factor which pushes the fictitious surface away from the solar field
 
 # Time parameters
 timezoneOffset = dt.timedelta(hours = 9.5)
@@ -65,6 +59,7 @@ while date <= end_date:
     datetime_list.append(date)
     date += timedelta(hours=1)
 num_timesteps = len(datetime_list)
+print('Datetime list done')
 
 # File paths to data sheets
 file_params = heliosoil_path + "/woomera_demo/parameters.xlsx"
@@ -76,42 +71,64 @@ physical_model = smb.physical_base()
 physical_model.import_site_data_and_constants(file_params)
 
 # Finding the tilt angles for all heliostats across the whole year
-tilt_angles_deg = np.zeros((num_heliostats, num_timesteps))
+print('Finding all heliostat tilts...')
+tilt_angles_rad = np.zeros((num_heliostats, num_timesteps))
 for i,date in enumerate(datetime_list):
 
     # Finding the position of the sun
     elevation_deg = solar.get_altitude(lat,lon,date) 
-    azimuth_deg = solar.get_azimuth(lat,lon,date) 
-    zenith_deg = 90 - elevation_deg  
-    elevation_rad, azimuth_rad, zenith_rad = (np.deg2rad(x) for x in [elevation_deg, azimuth_deg, zenith_deg])
 
-    sun_position = np.array([np.sin(azimuth_rad)*np.sin(zenith_rad), np.cos(azimuth_rad)*np.sin(zenith_rad), np.cos(zenith_rad)])
+    if elevation_deg > 0:
+        azimuth_deg = solar.get_azimuth(lat,lon,date) 
+        zenith_deg = 90 - elevation_deg  
+        elevation_rad, azimuth_rad, zenith_rad = (np.deg2rad(x) for x in [elevation_deg, azimuth_deg, zenith_deg])
 
-    sn = sun_position[0:3]/np.linalg.norm(sun_position[0:3])
-    theta_T = np.arctan(sn[0]/sn[2])
+        sun_position = np.array([np.sin(azimuth_rad)*np.sin(zenith_rad), np.cos(azimuth_rad)*np.sin(zenith_rad), np.cos(zenith_rad)])
+        sn = sun_position[0:3]/np.linalg.norm(sun_position[0:3])
+        theta_T = np.arctan(sn[0]/sn[2])
 
-    theta_aim = calculate_theta_aim(Xaim=receiver_position[0], Zaim=receiver_position[2], X0=heliostat_position[0], Z0=heliostat_position[2])
-    tilt = calculate_tilt(theta_T, theta_aim)
-    panel_normal = calculate_panel_normal(tilt)
+        # Finding the tilt of the of the heliostat according to the position of the sun
+        for p, x_position in enumerate(panel_positions):
+            theta_aim = calculate_theta_aim(Xaim=receiver_position[0], Zaim=receiver_position[2], X0=x_position, Z0=panel_height)
+            tilt_angles_rad[p][i] = calculate_tilt(theta_T, theta_aim)
+    else:
+        for p, x_position in enumerate(panel_positions):
+            tilt_angles_rad[p][i] = np.pi/2
+    
+print('All tilt calculations done')
 
+#%%
+# Finding the equivalent reflectivity of each panel for each hour of the year
+panel_reflectivities = np.zeros((num_heliostats, num_timesteps))
+sigma_dep = 0.01
 
+physical_model.helios.tilt = {0: np.rad2deg(tilt_angles_rad)}
+physical_model.helios.compute_extinction_weights(sim_data, loss_model="geometry", verbose=True)
+physical_model.deposition_flux(sim_data, hrz0=physical_model.hrz0, verbose=True)
+physical_model.calculate_delta_soiled_area(sim_data, sigma_dep=sigma_dep, verbose=True)
+delta_soiled_area = physical_model.helios.delta_soiled_area[0]
+cumulative_soiled_area = np.cumsum(delta_soiled_area, axis=1)
 
+#%%
+# Plotting the soiling of the heliostats over the year
+time = np.arange(num_timesteps)
 
+# Plot the delta_soiled_area for each heliostat
+plt.figure(figsize=(12, 8))
 
+for i in range(num_heliostats):
+    plt.plot(time, cumulative_soiled_area[i, :], label=f"Heliostat {i+1}", linewidth=1.5)
 
-# # Example heliostat setup. 11 Heliostats, each at a certain angle during the whole year.
-# physical_model.helios.tilt = {0: np.tile(fixed_tilt_angles, (num_timesteps, 1)).T}
+# Add labels and title
+plt.xlabel("Time (hours)")
+plt.ylabel("Soiled Area (m²/m²)")
+plt.title("Soiling of Heliostats Over Time")
+plt.legend(loc='upper left')
+plt.grid(True)
 
-# # Compute optical extinction weights
-# physical_model.helios.compute_extinction_weights(sim_data, loss_model="geometry", verbose=True)
+# Show the plot
+plt.tight_layout()
+plt.show()
 
-# # Calculate deposition flux to populate pdfqN
-# physical_model.deposition_flux(sim_data, hrz0=physical_model.hrz0, verbose=True)
-
-# # Calculate delta soiled area
-# sigma_dep = 0.01
-# physical_model.calculate_delta_soiled_area(sim_data, sigma_dep=sigma_dep, verbose=True)
-
-# # Access and analyze results
-# delta_soiled_area = physical_model.helios.delta_soiled_area[0]
+#%%
 
